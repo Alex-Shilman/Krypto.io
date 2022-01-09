9252d181f30c8fbf8cb07b62db5d5822d921029a
Alex Shilman <ashilman@cainc.com>
2022-01-09 00:51:46 -0500

    refactored toggler

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -20,6 +20,7 @@ const Navbar = () => {
         'Tutorials',
         'Wallets',
     ];
+    const ToggleButton = toggleMenu ? AiOutlineClose : HiMenuAlt4;
     return (
         <nav className="w-full flex md:justify-center justify-between items-center p-4">
             <div className="md:flex-[0.5] flex-initial justify-center items-center">
@@ -30,14 +31,14 @@ const Navbar = () => {
                 <li className="bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]">Login</li>
             </ul>
             <div className="flex relative">
-                {
-                    toggleMenu
-                        ? <AiOutlineClose fontSize="28" className="text-white md:hidden cursor-pointer z-20" onClick={() => setToggleMenu(false)}/>
-                        : <HiMenuAlt4 fontSize="28" className="text-white md:hidden cursor-pointer z-20" onClick={() => setToggleMenu(true)}/>
-                }
+                <ToggleButton 
+                    fontSize="28" 
+                    className="text-white md:hidden cursor-pointer z-20" 
+                    onClick={() => setToggleMenu(!toggleMenu)}
+                />
                 {
                     toggleMenu && (
-                        <ul className="z-10 fixed top-0 -right-2 p-3 w-[50vw] h-screen shadow-2xl md:hidden list-none flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in">
+                        <ul className="z-10 fixed top-0 -right-2 p-3 w-[100vw] h-screen shadow-2xl md:hidden list-none flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in">
                             <li className="text-xl w-full my-2 invisible">
                                 <AiOutlineClose onClick={() => setToggleMenu(false)}/>
                             </li>
